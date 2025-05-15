0e0fed0df9a5d39f7e2f74b340768467b46b0d39
Realion033 <purumisky@gmail.com>
2025-05-15 17:28:10 +0900

    feat : Init

--- MG/Source/MG/Manager/MGTileManager.cpp
+++ MG/Source/MG/Manager/MGTileManager.cpp
@@ -8,7 +8,6 @@ AMGTileManager::AMGTileManager()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
@@ -16,12 +15,23 @@ void AMGTileManager::BeginPlay()
 {
 	Super::BeginPlay();
 	// UE_LOG(LogTemp, Display, TEXT("[AMG Manager] : Begin play"));
+	Init();
 }
+
+void AMGTileManager::Init()
+{
+	for (int32 i = 0; i < MaxRows; i++)
+	{
+		for (int32 j = 0; j < MaxCols; j++)
+		{
+			
+		}
+	}
+}
+
 
 // Called every frame
 void AMGTileManager::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
-
--- MG/Source/MG/Manager/MGTileManager.h
+++ MG/Source/MG/Manager/MGTileManager.h
@@ -6,6 +6,14 @@
 #include "GameFramework/Actor.h"
 #include "MGTileManager.generated.h"
 
+USTRUCT()
+struct FTileRow
+{
+	GENERATED_BODY()
+	UPROPERTY()
+	TArray<AActor*> Row;
+};
+
 UCLASS()
 class MG_API AMGTileManager : public AActor
 {
@@ -22,8 +30,14 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
+	void Init();
+	
+private:
+	static const int32 MaxRows = 10;
+	static const int32 MaxCols = 10;
 private:
 	UPROPERTY(EditAnywhere)
 	TSubclassOf<AActor> TileActorClass;
+	UPROPERTY(EditAnywhere)
+	TArray<FTileRow> Tiles;
 };
